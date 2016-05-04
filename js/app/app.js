7b1f56b1ba4727ca397426d9ec2a4f9733f98163
Nukul <nukul.ghosh@io-media.com>
2016-05-04 18:41:29 +0530

    login page updation

--- js/app/app.js
+++ js/app/app.js
@@ -2,12 +2,17 @@ var app = angular.module('angularCarousel', ['ui.router']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/accordion');
+    $urlRouterProvider.otherwise('/login');
 
     $stateProvider
       .state('accordion', {
           url:'/accordion',
           templateUrl:'templates/carousel.html',
           controller:'carouselCTRL'
+      })
+      .state('login', {
+      		url:'/login',
+      		templateUrl:'templates/login.html',
+      		controller:'loginCTRL'
       });
 }]);
---
+++ js/app/loginCTRL.js
@@ -0,0 +1,3 @@
+app.controller('loginCTRL', ['$scope', function($scope){
+	
+}]);
